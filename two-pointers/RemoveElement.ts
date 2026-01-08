da47ff06daebe416a4830bdd7a16c8e1b76e5869
Yetty2020 <fatihaadetoro@gmail.com>
2026-01-08 22:13:25 +0100

    Function that removes some characters from a given array

--- two-pointers/RemoveElement.ts
+++ two-pointers/RemoveElement.ts
@@ -1,9 +1,19 @@
+/*
+Problem: Write a functiont that removes element in an array thats not equal to a given number (val) and returns the amount of characters left in the array , for example num = [1,2,3,2,2] val = 2, output = 2
+ */
+
+
 function removeElement(nums: number[], val: number): number {
+    // this serves as the counter for the number of items in the array
     let k = 0;
 
+    //we loop through the given no
+
     for (let i = 0; i < nums.length; i++) {
+        // as long as the current iteration number is not equal to the variable given
         if (nums[i] !== val) {
             nums[k] = nums[i];
+            // to increment the counter
             k++;
         }
     }
