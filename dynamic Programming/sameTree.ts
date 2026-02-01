63b69f890e770101d871d890f25d0ff5e921118b
Yetty2020 <fatihaadetoro@gmail.com>
2026-02-01 23:21:59 +0100

    Definition of binary tree node

--- dynamic Programming/sameTree.ts
+++ dynamic Programming/sameTree.ts
@@ -1,1 +1,19 @@
-/*Function to  check if two binary tree node are the sae */
+/*Function to  check if two binary tree node are the same */
+
+/*
+Approach is to first check if the node has the same value, then if left subtree are the same and then if the right subtree are the same, then you treat each subtree as a tree on its own through recursion */
+
+class TreeNode{
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null){
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean{
+    
+}
