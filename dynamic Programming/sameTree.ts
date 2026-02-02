d9f10ca9d0ee72a6983b20c4806ae3252e71335c
Yetty2020 <fatihaadetoro@gmail.com>
2026-02-02 21:38:33 +0100

    Function to check if two binary tree are the same

--- dynamic Programming/sameTree.ts
+++ dynamic Programming/sameTree.ts
@@ -15,5 +15,13 @@ class TreeNode{
 }
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean{
-    
+    //check if both nodes are null, then they are the same
+    if (p === null && q === null) return true
+    // check if one of the nodes is null, then they are not the same
+    if (p === null || q === null) return false
+    //check if the values of the nodes are the same
+    if (p.val !== q.val) return false
+
+    // in a binary tree, each node or treee consists of subtress, left and right. So we treat each subtree as a tree on its own and check if the nodes are the same using recursion
+    return isSameTree(p.left, q.left) && isSameTree(p.right,q.right)
 }
