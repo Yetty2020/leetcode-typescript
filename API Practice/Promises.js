fe6675cd34c0a673a75c5ae1a2bfff4e93a33de0
Yetty2020 <fatihaadetoro@gmail.com>
2026-03-11 23:33:33 +0100

    API fetch update

--- API Practice/Promises.js
+++ API Practice/Promises.js
@@ -13,3 +13,49 @@ let p = new Promise((resolve, reject) =>{
 
     }
 })
+
+//we handle the results from the promise using .then and .catch. These takes in message parameter which we can use to show the results from the promise
+p.then((message) =>{
+    console.log("This is in the then " + message)
+}).catch((message) =>{
+    console.log("This is in the catch " + message)
+})
+
+
+// Another Example of promise
+const userLeft = false
+const userWatchingCatMeme = false
+
+// we create a function that returns a promise.
+function watchTutorialPromise(){
+    return new Promise((resolve, reject) =>{
+
+        //we check if the user is left or watching cat meme, either of these being true is a reject case
+        if (userLeft){
+            reject({
+                name: "User left",
+                message:":("
+            })
+        } else if (userWatchingCatMeme){
+            reject({
+                name: "User is watching meme",
+                message: "WebDevSimplified < Cat"
+            })
+            // This is a resolve case
+        } else {
+            resolve("Tutorial completed successfully")
+        }
+
+    })
+}
+
+// we call back the above promise here
+// this always takes in the message paraeter which can be used to show the results of the promise
+watchTutorialPromise().then((message) =>{
+    console.log("This is in the then " + message)
+
+
+}). catch((message)=>{
+    console.log(message.name + " " + message.message)
+
+})
